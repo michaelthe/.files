58948ae77611db37a794fd7426646b77e0c1dd78
michael theodorides <mc.theodorides@gmail.com>
2019-08-11 11:19:10 +0300

    update

--- backup.sh
+++ backup.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "$#" -ne 2 ]]; then
     echo "Usage: $0 src dest-dir"
 else
-    sudo rsync --archive --delete --progress --no-links --exclude-from="${DIR}/.rsync-exclude" "$1" "$2"
+    sudo rsync --archive --progress --exclude-from="${DIR}/.rsync-exclude" "$1" "$2"
 fi
